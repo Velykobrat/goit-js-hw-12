eea551ff4710f483f97278396e6e653d4432f4b4
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-04-20 09:36:18 +0300

    бібліотека Axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,17 @@
-// Функція для виконання запиту до API Pixabay
+import axios from 'axios'; // Імпортуємо Axios замість fetch
+
+// Оновлена функція для виконання запиту до API Pixabay з використанням Axios
 export async function fetchImages(searchQuery) {
     const apiKey = '43059810-21766dfeafea29ca9c24ae0e2';
     const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
   
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data.hits;
+      const response = await axios.get(url); // Використовуємо метод axios.get
+      return response.data.hits; // Повертаємо дані з об'єкту response.data
     } catch (error) {
       console.error('Error fetching data:', error);
       return [];
     }
-  }
+}
+
+// Видалення імпорту fetchImages, оскільки ця функція тепер імпортується з цього файлу
